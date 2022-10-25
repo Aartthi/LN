c992be78ede8558c1b263454157391413e5da76f
“Aartthi” <Aartthiit@gmail.com>
2022-10-25 14:00:49 +0000

    2nd save

--- law.py
+++ law.py
@@ -5,9 +5,9 @@ b = float(input('Enter b: '))
 c = float(input('Enter c: '))  
   
 # calculate the discriminant  
-d = (b**2) - (4*a*c)  
+d = (b**4) - (4*a*c)  
   
 # find two solutions  
 sol1 = (-b-cmath.sqrt(d))/(2*a)  
 sol2 = (-b+cmath.sqrt(d))/(2*a)  
-print('The solution are {0} and {1}'.format(sol1,sol2))  
+print('The solution are {0} and {1}'.format(sol1,sol2))
